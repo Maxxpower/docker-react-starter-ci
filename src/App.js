5b45b3be1924d40f09abeda35bf7a3f8031ece12
fimperioli <fimperioli@LAPTOP-5I7C3NOH.localdomain>
2021-05-17 16:00:03 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hi There Fra You're using docker!
+         Hi There Fra You're using docker, pull merged!
         </p>
         <a
           className="App-link"
